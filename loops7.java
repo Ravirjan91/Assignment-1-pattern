f669deada0a32cdd680872652a5dc50ae598a538
RAVI RANJAN <129931451+Ravirjan91@users.noreply.github.com>
2023-05-06 12:31:32 +0530

    Add files via upload
    x star pattern

---
+++ loops7.java
@@ -0,0 +1,24 @@
+package loops;
+
+public class loops7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7;
+		
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(i==j || i+j==(n-1))
+			System.out.print("* ");
+				else
+			System.out.print("  ");
+					
+			}
+			System.out.println();
+		}
+
+	}
+ 
+}
